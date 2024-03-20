97dfd2c93df5c0da508aa209b4a3636ee9076da6
Md Maruf Hosen <57535976+Maruf-16203091@users.noreply.github.com>
2024-03-20 15:30:48 +0100

    changing the header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,48 +1,42 @@
 import React from "react";
-import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { AppBar, Toolbar, Button, Box, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
+import Logo from "../assets/logo.png"; // Import your logo image
 
 const Header = () => {
   return (
-    <AppBar position="fixed" sx={{ backgroundColor: "#07784b", width: "100%" }}>
+    <AppBar position="fixed" sx={{ backgroundColor: "#ffffff", width: "100%" }}>
       <Toolbar
         sx={{
-          paddingLeft: "20px",
-          paddingRight: "20px",
+          display: "flex",
           justifyContent: "space-between",
+          alignItems: "center",
+          paddingX: "20px",
         }}
       >
-        {" "}
-        {/* Add padding on both sides and align items */}
-        <Typography
-          variant="h6"
-          component={Link}
-          to="/"
-          sx={{
-            color: "white",
-            fontWeight: "bold",
-            textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)",
-            "&:hover": {
-              color: "white",
-            },
-          }}
-        >
-          Booking
-        </Typography>
+        <Box>
+          <Link to="/">
+            <img
+              src={Logo}
+              alt="Logo"
+              style={{ width: "120px", height: "auto", marginTop: "15px" }}
+            />
+          </Link>
+        </Box>
         <Box>
           <Button
             component={Link}
             to="/about"
             color="inherit"
             sx={{
-              color: "white",
+              color: "#2a2a2e",
               fontWeight: "bold",
               marginLeft: "10px",
               "&:hover": {
                 backgroundColor: "rgba(255, 255, 255, 0.2)",
-                color: "white", // Change background color with opacity on hover
+                color: "#2a2a2e",
               },
-            }} // Add margin for spacing
+            }}
           >
             About
           </Button>
@@ -51,14 +45,14 @@ const Header = () => {
             to="/services"
             color="inherit"
             sx={{
-              color: "white",
+              color: "#2a2a2e",
               fontWeight: "bold",
               marginLeft: "10px",
               "&:hover": {
                 backgroundColor: "rgba(255, 255, 255, 0.2)",
-                color: "white", // Change background color with opacity on hover
+                color: "#2a2a2e",
               },
-            }} // Add margin for spacing
+            }}
           >
             Services
           </Button>
@@ -67,14 +61,14 @@ const Header = () => {
             to="/contact"
             color="inherit"
             sx={{
-              color: "white",
+              color: "#2a2a2e",
               fontWeight: "bold",
               marginLeft: "10px",
               "&:hover": {
                 backgroundColor: "rgba(255, 255, 255, 0.2)",
-                color: "white", // Change background color with opacity on hover
+                color: "#2a2a2e",
               },
-            }} // Add margin for spacing
+            }}
           >
             Contact
           </Button>
