beb30ae197107ba847285112bd2e7105abc95682
Md Maruf Hosen <57535976+Maruf-16203091@users.noreply.github.com>
2024-03-25 16:13:44 +0100

    Add css class to righ nav

--- src/components/header/navBar/rightNav/RightNav.jsx
+++ src/components/header/navBar/rightNav/RightNav.jsx
@@ -14,6 +14,7 @@ import CurrencyModal from "../../../modals/currencyModal/CurrencyModal";
 import AddShoppingCartOutlinedIcon from "@mui/icons-material/AddShoppingCartOutlined";
 import MenuIcon from "@mui/icons-material/Menu";
 import React, { useState } from "react";
+import "./RightNav.css";
 
 const RightNavBar = () => {
   const [isHovered, setIsHovered] = useState(false);
