4d9c8b8b5e02d94b4379c4c52051591c518b7dff
Md Maruf Hosen <57535976+Maruf-16203091@users.noreply.github.com>
2024-03-31 03:12:16 +0200

    adjust the rating

--- src/components/others/Description.jsx
+++ src/components/others/Description.jsx
@@ -2,22 +2,86 @@ import React from "react";
 import { Typography } from "@mui/material";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 
-const Description = ({ amount, location, price, currency, rating }) => {
+const Description = ({ text, amount, location, price, currency, rating }) => {
   return (
-    <div style={{  position: "absolute" }}>
-      <div style={{ display: "flex", alignItems: "center" }}>
-        <LocationOnIcon style={{ marginRight: "2px" }} />
-        <Typography variant="body2" color="textSecondary" component="p">
-        {location}
+    <div
+      style={{
+        position: "absolute",
+        fontSize: "12px",
+        fontFamily: "mallory, Helvetica Neue, Helvetica, Arial, sans-sarif",
+      }}
+    >
+      <Typography
+        variant="body2"
+        color="textSecondary"
+        component="p"
+        sx={{
+          fontSize: "12px",
+          fontFamily: "mallory, Helvetica Neue, Helvetica, Arial, sans-sarif",
+          fontWeight: "700",
+          marginBottom: "5px",
+        }}
+      >
+        {text}
+      </Typography>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          fontSize: "12px",
+          fontFamily: "mallory, Helvetica Neue, Helvetica, Arial, sans-sarif",
+        }}
+      >
+        <LocationOnIcon style={{ marginRight: "2px", marginLeft: "-9px" }} />
+        <Typography
+          variant="body2"
+          color="textSecondary"
+          component="p"
+          sx={{
+            fontSize: "12px",
+            fontFamily: "mallory, Helvetica Neue, Helvetica, Arial, sans-sarif",
+            color: "#8BC0FB",
+          }}
+        >
+          {location}
         </Typography>
       </div>
-      <Typography variant="body2" color="textSecondary" component="p">
+      <Typography
+        variant="body2"
+        color="#FF567D"
+        component="p"
+        sx={{
+          fontSize: "30px",
+          fontFamily: "mallory, Helvetica Neue, Helvetica, Arial, sans-sarif",
+          marginBottom: "-15px",
+        }}
+      >
         {rating}
       </Typography>
-      <Typography variant="body2" color="textSecondary" component="p">
-        {price} 
+      <Typography
+        variant="body2"
+        color="textSecondary"
+        component="p"
+        sx={{
+          fontSize: "12px",
+          fontFamily: "mallory, Helvetica Neue, Helvetica, Arial, sans-sarif",
+        }}
+      >
+        {price}
       </Typography>
-      {currency} {amount}
+      <Typography
+        variant="body2"
+        color="textSecondary"
+        component="p"
+        sx={{
+          fontSize: "12px",
+          fontFamily: "mallory, Helvetica Neue, Helvetica, Arial, sans-sarif",
+          color: "#E12D8C",
+          fontWeight: "700",
+        }}
+      >
+        {currency} {amount}
+      </Typography>
     </div>
   );
 };
