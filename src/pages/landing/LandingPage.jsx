c5d67c79e23c8fb7aa157567ad3e2568acf7af6e
Md Maruf Hosen <57535976+Maruf-16203091@users.noreply.github.com>
2024-03-31 17:32:36 +0200

    overhead section  added

--- src/pages/landing/LandingPage.jsx
+++ src/pages/landing/LandingPage.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import TopDestination from '../landing/topDestination/TopDestination'
+import TopDestination from "../landing/topDestination/TopDestination";
 import PromotionPage from "../landing/promotion/AccomoPromotion";
+import OverHeard from "../landing/overheard/Overheard";
 import Featured from "../landing/featured/Featured";
 
 const LandingPage = () => {
   return (
     <>
       <TopDestination />
-      <Featured/>
+      <Featured />
+      <OverHeard />
     </>
   );
 };
---
+++ src/pages/landing/overheard/Overheard.jsx
@@ -0,0 +1,93 @@
+import React from "react";
+import { Card, CardContent, Typography, Link } from "@mui/material";
+
+const OverHeard = () => {
+  return (
+    <>
+      <div
+        style={{
+          maxWidth: "900px",
+          position: "relative",
+          textAlign: "center",
+        }}
+      >
+        <Typography
+          variant="h5"
+          gutterBottom
+          sx={{
+            fontFamily: "mallory, Helvetica Neue, Helvetica, Arial, sans-sarif",
+            marginBottom: "20px",
+            marginLeft: "150px",
+            fontWeight: "700",
+            alignItems: "center",
+            fontSize: "26px",
+          }}
+        >
+          Overheard from travelers
+        </Typography>
+      </div>
+      <div
+        style={{
+          maxWidth: "900px",
+          marginBottom: "150px",
+          marginLeft: "20px",
+          position: "relative",
+          textAlign: "center",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        {[1, 2, 3].map((index) => (
+          <Card
+            key={index}
+            style={{ width: "300px", height: "300px", margin: "20px" }}
+          >
+            <CardContent>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "#5392f9",
+                  fontFamily:
+                    "mallory, Helvetica Neue, Helvetica, Arial, sans-sarif",
+                }}
+              >
+                <Link href="#" underline="none">
+                  Baiyoke Suite Hotel
+                </Link>
+              </Typography>
+              <Typography
+                variant="body1"
+                gutterBottom
+                sx={{
+                  marginBottom: "40px",
+                  fontFamily:
+                    "mallory, Helvetica Neue, Helvetica, Arial, sans-sarif",
+                }}
+              >
+                in India
+              </Typography>
+              <Typography
+                variant="body1"
+                gutterBottom
+                sx={{
+                  fontFamily:
+                    "mallory, Helvetica Neue, Helvetica, Arial, sans-sarif",
+                  marginBottom: "50px",
+                  fontSize: "20px",
+                  fontWeight: "700",
+                }}
+              >
+                "I will keep on recommending Agoda to my friends."
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                - Amit from India
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default OverHeard;
