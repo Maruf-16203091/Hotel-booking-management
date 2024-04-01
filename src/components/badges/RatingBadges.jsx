f4e08d2fa6c92091b30a954f9d1b513246d0f30a
Md Maruf Hosen <57535976+Maruf-16203091@users.noreply.github.com>
2024-04-01 07:04:35 +0200

    change the style of rating badge

--- src/components/badges/RatingBadges.jsx
+++ src/components/badges/RatingBadges.jsx
@@ -3,22 +3,38 @@ import { Typography, Box } from "@mui/material";
 
 const RatingBadge = ({ rating }) => {
   return (
-    <Box
-      sx={{
-        position: "absolute",
-        top: "10px",
-        right: "10px",
-        backgroundColor: "#5392F9",
-        borderStyle: "solid",
-        borderWidth: "0px 10px 0 4px",
-        borderColor: "#5392F9 transparent transparent transparent",
-        color: "white",
-        padding: "5px",
-        borderRadius: "5px ",
-      }}
-    >
-      {rating}
-    </Box>
+    <>
+      <Box
+        sx={{
+          position: "absolute",
+          top: "10px",
+          right: "22px",
+          backgroundColor: "#5392F9",
+          borderStyle: "solid",
+          borderWidth: "0px 10px 0 4px",
+          borderColor: "#5392F9 transparent transparent transparent",
+          color: "white",
+          padding: "5px",
+          borderRadius: "5px ",
+        }}
+      >
+        {rating}
+      </Box>
+      <span
+        style={{
+          position: "absolute",
+          bottom: "231px",
+          right: "-5px",
+          width: "0",
+          height: "0",
+          borderBottom: "25px solid transparent",
+          borderTop: "0px solid transparent",
+          borderLeft: "16px solid #5392F9",
+          
+          borderRight: "16px solid transparent",
+        }}
+      />
+    </>
   );
 };
 
