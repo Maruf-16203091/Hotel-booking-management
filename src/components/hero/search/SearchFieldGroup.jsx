bf7eb340fe731a2e822a07473eac7364c0de471d
Md Maruf Hosen <57535976+Maruf-16203091@users.noreply.github.com>
2024-04-02 23:28:36 +0200

    room selection field setup

--- src/components/hero/search/SearchFieldGroup.jsx
+++ src/components/hero/search/SearchFieldGroup.jsx
@@ -1,14 +1,61 @@
-import React from "react";
-import { Box, TextField, Divider } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Box,
+  TextField,
+  Divider,
+  InputAdornment,
+  Button,
+  Card,
+  CardContent,
+  Typography,
+} from "@mui/material";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import LoginIcon from "@mui/icons-material/Login";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import GroupIcon from "@mui/icons-material/Group";
 
 const SearchFieldGroup = ({ selectedDate, setSelectedDate }) => {
+  const [isCardOpen, setIsCardOpen] = useState(false);
+  const [roomCount, setRoomCount] = useState(1);
+  const [adultCount, setAdultCount] = useState(1);
+  const [childCount, setChildCount] = useState(0);
+
+  const handleIncrease = (type) => {
+    switch (type) {
+      case "room":
+        setRoomCount(roomCount + 1);
+        break;
+      case "adult":
+        setAdultCount(adultCount + 1);
+        break;
+      case "child":
+        setChildCount(childCount + 1);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const handleDecrease = (type) => {
+    switch (type) {
+      case "room":
+        setRoomCount(roomCount > 1 ? roomCount - 1 : 1);
+        break;
+      case "adult":
+        setAdultCount(adultCount > 0 ? adultCount - 1 : 0);
+        break;
+      case "child":
+        setChildCount(childCount > 0 ? childCount - 1 : 0);
+        break;
+      default:
+        break;
+    }
+  };
   return (
     <Box
       sx={{
+        position: "relative",
         width: "1100px",
         height: "65px",
         backgroundColor: "#F8F7F9",
@@ -70,7 +117,15 @@ const SearchFieldGroup = ({ selectedDate, setSelectedDate }) => {
 
       <TextField
         fullWidth
-        placeholder="Field 2"
+        placeholder="1 adult\n1 room"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <GroupIcon />
+            </InputAdornment>
+          ),
+          onClick: () => setIsCardOpen(true),
+        }}
         sx={{
           marginLeft: "45px",
           borderRadius: "12px",
@@ -89,10 +144,93 @@ const SearchFieldGroup = ({ selectedDate, setSelectedDate }) => {
               color: "black",
               fontFamily:
                 "mallory, Helvetica Neue, Helvetica, Arial, sans-sarif",
+              whiteSpace: "pre-line",
             },
           },
         }}
       />
+      {isCardOpen && (
+        <Card
+          sx={{
+            position: "absolute",
+            top: 80,
+            left: 578,
+            width: "280px",
+            height: "150px",
+            borderRadius: "12px",
+            zIndex: "1",
+          }}
+        >
+          <CardContent>
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Typography>Room</Typography>
+              <Box display="flex" alignItems="center">
+                <Button
+                  variant="contained"
+                  onClick={() => handleDecrease("room")}
+                >
+                  -
+                </Button>
+                <Typography sx={{ margin: "0 10px" }}>{roomCount}</Typography>
+                <Button
+                  variant="contained"
+                  onClick={() => handleIncrease("room")}
+                >
+                  +
+                </Button>
+              </Box>
+            </Box>
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Typography>Adult</Typography>
+              <Box display="flex" alignItems="center">
+                <Button
+                  variant="contained"
+                  onClick={() => handleDecrease("adult")}
+                >
+                  -
+                </Button>
+                <Typography sx={{ margin: "0 10px" }}>{adultCount}</Typography>
+                <Button
+                  variant="contained"
+                  onClick={() => handleIncrease("adult")}
+                >
+                  +
+                </Button>
+              </Box>
+            </Box>
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Typography>Children</Typography>
+              <Box display="flex" alignItems="center">
+                <Button
+                  variant="contained"
+                  onClick={() => handleDecrease("child")}
+                >
+                  -
+                </Button>
+                <Typography sx={{ margin: "0 10px" }}>{childCount}</Typography>
+                <Button
+                  variant="contained"
+                  onClick={() => handleIncrease("child")}
+                >
+                  +
+                </Button>
+              </Box>
+            </Box>
+          </CardContent>
+        </Card>
+      )}
     </Box>
   );
 };
