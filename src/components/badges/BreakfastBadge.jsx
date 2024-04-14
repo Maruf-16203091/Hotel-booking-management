f43e5b15ca32040c369be432c3ba903329dba7f2
Md Maruf Hosen <57535976+Maruf-16203091@users.noreply.github.com>
2024-04-15 00:51:05 +0200

    breakfastbadge built

---
+++ src/components/badges/BreakfastBadge.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Typography, Box } from "@mui/material";
+
+const BreakfastBadge = () => {
+  return (
+    <Box>
+      <Typography
+        variant="body2"
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          textAlign: "center",
+          position: "absolute",
+          top: "-380px",
+          right: "50%",
+          left: "385px",
+          marginTop: "800px",
+          transform: "translate(50%, -50%)",
+          padding: "5px",
+          paddingTop: "5px",
+          backgroundColor: "#E02D2D",
+          color: "#ffffff",
+          borderRadius: "4px",
+          fontFamily: "mallory, Helvetica Neue, Helvetica, Arial, sans-sarif",
+          fontSize: "10px",
+          fontWeight: "700",
+          width: "80px",
+          height: "15px",
+        }}
+      >
+        Breakfast Included
+      </Typography>
+    </Box>
+  );
+};
+
+export default BreakfastBadge;
