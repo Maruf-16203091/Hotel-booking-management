bbde06f99087aad54321597f1302d2bc3a1a1e95
Md Maruf Hosen <57535976+Maruf-16203091@users.noreply.github.com>
2024-04-17 12:07:47 +0200

    create update button

---
+++ src/components/buttons/Update.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Box, Button } from "@mui/material";
+
+const UpdateButton = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        marginTop: "70px",
+        marginLeft: "60px",
+      }}
+    >
+      <Button
+        variant="contained"
+        sx={{
+          width: "340px",
+          height: "60px",
+          position: "relative",
+          border: "none",
+          userSelect: "none",
+          padding: "12px",
+          borderRadius: "10px",
+          backgroundColor: "#5392f9",
+          color: "#FFFFFF",
+          boxShadow: "rgba(0, 0, 0, 0.2) 0px 1px 3px 1px",
+          transition: "all 0.15s ease-in-out 0s",
+          fontSize: "22px",
+          fontFamily: "mallory, Helvetica Neue, Helvetica, Arial, sans-sarif",
+        }}
+      >
+        SEARCH
+      </Button>
+    </Box>
+  );
+};
+
+export default UpdateButton;
