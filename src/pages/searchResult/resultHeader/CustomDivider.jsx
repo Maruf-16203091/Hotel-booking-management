a94703700077f9a2550f8bf4f125095db547f929
Md Maruf Hosen <57535976+Maruf-16203091@users.noreply.github.com>
2024-05-04 22:41:22 +0200

    CustomDivider completed

--- src/pages/searchResult/resultHeader/CustomDivider.jsx
+++ src/pages/searchResult/resultHeader/CustomDivider.jsx
@@ -3,22 +3,43 @@ import { Divider, Typography } from "@mui/material";
 
 const CustomDivider = () => {
   return (
-    <Divider
+    <div
       style={{
-        width: "908px",
-        marginTop: "80px",
-        marginLeft: "622px",
-        marginBottom: "-95px",
+        width: "100%",
+        marginTop: "100px",
+        textAlign: "center",
+        marginLeft: "475px",
+        marginBottom: "-80px",
       }}
     >
-      <Typography variant="body2" color="text.secondary" sx={{ p: 2 }}>
+      <Divider
+        style={{
+          width: "15%",
+          display: "inline-block",
+          verticalAlign: "middle",
+          borderBottom: "2px solid #cacbcc",
+        }}
+      />
+      <Typography
+        variant="body2"
+        color="text.secondary"
+        sx={{ display: "inline-block", padding: "0 10px" }}
+      >
         Commission earned affects the order of your results.{" "}
         <Typography component="a" href="#" color="primary" underline="always">
           Learn more about ranking
         </Typography>
         .
       </Typography>
-    </Divider>
+      <Divider
+        style={{
+          width: "15%",
+          display: "inline-block",
+          verticalAlign: "middle",
+          borderBottom: "2px solid #cacbcc", // Adjust thickness here
+        }}
+      />
+    </div>
   );
 };
 
