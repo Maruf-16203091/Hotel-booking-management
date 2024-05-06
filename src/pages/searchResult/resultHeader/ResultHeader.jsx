b2f58f6e386ba4aaa75592204e5f463e8727fd19
Md Maruf Hosen <57535976+Maruf-16203091@users.noreply.github.com>
2024-05-06 13:53:38 +0200

    tab customised

--- src/pages/searchResult/resultHeader/ResultHeader.jsx
+++ src/pages/searchResult/resultHeader/ResultHeader.jsx
@@ -1,56 +1,58 @@
 import React, { useState } from "react";
-import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem, Tab, Tabs } from "@mui/material";
-import MenuIcon from "@mui/icons-material/Menu";
+import { AppBar, Toolbar, Tabs, Tab } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  tab: {
+   
+    marginRight: "5px",
+    "&.Mui-selected": {
+      backgroundColor: "#5392F9", // Background color on tab click
+      color: "#fff", // Text color on tab click
+    },
+  },
+});
 
 const ResultHeader = () => {
-  const [anchorEl, setAnchorEl] = useState(null);
-  const [selectedTab, setSelectedTab] = useState(0);
-
-  const handleMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
+  const [selectedTab, setSelectedTab] = useState(1); // Initialize to 1 for "Our Picks"
+  const classes = useStyles();
 
   const handleChangeTab = (event, newValue) => {
     setSelectedTab(newValue);
   };
 
   return (
-    <>
-      <AppBar
-        position="static"
-        sx={{
-          backgroundColor: "#F8F7F9",
-          color: "black",
-          width: "76%",
-          marginTop: "60px",
-          borderRadius: "4px",
-          marginLeft: "619px",
-          marginBottom: "-80px",
-        }}
-      >
-        <Toolbar>
-          
-          <Tabs
-            value={selectedTab}
-            onChange={handleChangeTab}
-            aria-label="tabs options"
-            textColor="inherit"
-            indicatorColor="secondary"
-          >
-            <Tab label="Sort" disabled />
-            <Tab label="Our Picks" />
-            <Tab label="Top Reviewed" />
-            <Tab label="Lowest Price First" />
-            <Tab label="Distance" />
-            <Tab label="Hot Deals" />
-          </Tabs>
-        </Toolbar>
-      </AppBar>
-    </>
+    <AppBar
+      position="static"
+      sx={{
+        width: "76%",
+        marginTop: "60px",
+        borderRadius: "4px",
+        marginLeft: "619px",
+        marginBottom: "-80px",
+        color: "black",
+        backgroundColor: "#FFFFFF",
+      }}
+    >
+      <Toolbar>
+        <Tabs
+          value={selectedTab}
+          onChange={handleChangeTab}
+          aria-label="tabs options"
+          textColor="inherit"
+          fontWeight="600"
+        textTransform= "none" 
+
+        >
+          <Tab label="Sort" sx={{ backgroundColor: "#F8F7F9" }} disabled />
+          <Tab label="Our Picks" className={classes.tab} />
+          <Tab label="Top Reviewed" className={classes.tab} />
+          <Tab label="Lowest Price First" className={classes.tab} />
+          <Tab label="Distance" className={classes.tab} />
+          <Tab label="Hot Deals" className={classes.tab} />
+        </Tabs>
+      </Toolbar>
+    </AppBar>
   );
 };
 
